f6f492187eedbc069c5807416c81c7c089efef4f
RichaBharti0603 <richab820@gmail.com>
2025-08-01 21:44:06 +0530

    pull  request needed

--- roommate-ai-backend/database/mongodb.py
+++ roommate-ai-backend/database/mongodb.py
@@ -2,7 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URI = "mongodb://localhost:27017"
 client = AsyncIOMotorClient(MONGO_URI)
-
 db = client.roommateDB
 user_collection = db.users
 match_collection = db.matches
