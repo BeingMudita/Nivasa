ba4635ce96cf23b06aec3a906072b3f282299242
Mudita Jain <mudita21jain@gmail.com>
2025-08-02 08:39:29 +0530

    resolving git issues

--- server/models/user.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class UserCredentials(BaseModel):
-    email: str
-    password: str  # plaintext for now (but hash in real apps)
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -60,7 +60,6 @@ const Navigation = () => {
                 </Button>
               );
             })}
-
             {/* Login/Logout Button */}
             {!user ? (
               <Button
